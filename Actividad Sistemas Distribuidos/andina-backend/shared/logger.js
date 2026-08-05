148d33fd3fdf4a92b5ee5843401eed06d21eb112
SimonSalamanca <simonsalamanca.av@academia.umb.edu.co>
2026-08-04 21:03:24 -0500

    Delete Actividad Sistemas Distribuidos directory

--- Actividad Sistemas Distribuidos/andina-backend/shared/logger.js
+++
@@ -1,7 +0,0 @@
-function log(service, msg, extra) {
-  const ts = new Date().toISOString().split('T')[1].replace('Z', '');
-  const suffix = extra ? ' ' + JSON.stringify(extra) : '';
-  console.log(`[${ts}] [${service}] ${msg}${suffix}`);
-}
-
-module.exports = { log };
